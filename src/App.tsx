708d10003c79027c67aa420e36ecb11dcae79d68
natserract <benjaminstwo@gmail.com>
2021-10-01 13:31:51 +0800

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,8 @@ import Layout from './containers/Layout'
 
 import "./App.css";
 
-// export const store = configureStore();
-
 export default (props) => (
-    // <Provider store={store}>
     <Layout>
         {props.children}
     </Layout>
-    // </Provider>
 )
